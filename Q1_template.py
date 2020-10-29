1f6fe47aef55358d1ea0de8184602874dbb98dec
cs60021SDM <73641271+cs60021SDM@users.noreply.github.com>
2020-10-29 15:31:33 +0530

    Add files via upload

---
+++ Q1_template.py
@@ -0,0 +1,112 @@
+import matplotlib.pyplot as plt
+import random
+import time
+import numpy as np
+from sklearn import metrics
+from math import sqrt
+from sklearn.model_selection import train_test_split\
+
+def diffa(y, ypred,x):
+    return (y-ypred)*(-x)
+
+def diffb(y, ypred):
+    return (y-ypred)*(-1)
+
+def shuffle_data(x,y):
+    # shuffle x，y，while keeping x_i corresponding to y_i
+    seed = random.random()
+    random.seed(seed)
+    random.shuffle(x)
+    random.seed(seed)
+    random.shuffle(y)
+
+def get_batch_data(x, y, batch):
+    shuffle_data(x, y)
+    x_batch = x[0:batch]
+    y_batch = y[0:batch]
+    return [x_batch, y_batch]
+
+data = np.loadtxt('LinearRegdata.txt')
+x = data[:, 1]
+y = data[:, 2]
+for i in range(0,5):
+  print("x[",i,"] = ",x[i],",","y[",i,"] = ",y[i])
+
+# Normalize the data
+x_max = max(x)
+x_min = min(x)
+y_max = max(y)
+y_min = min(y)
+for i in range(0, len(x)):
+    x[i] = (x[i] - x_min)/(x_max - x_min)
+    y[i] = (y[i] - y_min)/(y_max - y_min)
+
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.3,random_state=109)
+
+
+def batch_gd(X_train, y_train):
+
+	a = 10.0
+	b = -20.0
+
+	all_bgdloss = []
+	all_ep = []
+
+	rate = 0.008
+	start = time.time()
+	for ep in range(1,100):
+	    loss = 0
+	    losst = 0
+	    all_da = 0
+	    all_db = 0
+	    for i in range(0, len(X_train)):
+	        y_pred = a*X_train[i] + b
+	        loss = loss + (y_train[i] - y_pred)*(y_train[i] - y_pred)/2
+	        all_da = all_da + diffa(y_train[i], y_pred, X_train[i]) #gradients accumulated
+	        all_db = all_db + diffb(y_train[i], y_pred) #gradients accumulated
+
+	    loss = loss/len(X_train)
+	    all_bgdloss.append(loss)
+	    all_ep.append(ep)
+	    plt.plot(all_ep, all_bgdloss, color = 'red')
+	    plt.title('BGD')
+	    plt.xlabel("Epoch")
+	    plt.ylabel("loss")
+
+	    #parameters updated
+	    a = a - rate * all_da
+	    b = b - rate * all_db
+
+	    #Saving best parameters    
+	    if ep==1:
+	        prevloss = losst
+	    else:
+	        if losst<prevloss:
+	          prevloss=losst
+	          param1 = a
+	          param2 = b
+
+	    if ep % 10 == 0:
+	        print("Epoch: ", ep, " Loss: ", loss)
+	        plt.show()
+	        plt.pause(0.01)
+
+
+def minibatch_gd():
+	pass
+
+def stochastic_gd():
+	pass
+
+def momentum_gd():
+	pass
+
+def adam_gd():
+	pass
+
+def main():
+
+	batch_gd(X_train, y_train)
+
+if __name__=='__main__':
+	main()
---
+++ Q2_template.py
@@ -0,0 +1,85 @@
+import torch
+from __future__ import print_function
+import argparse
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import matplotlib.pyplot as plt
+from torchvision import datasets, transforms
+from torch.optim.lr_scheduler import StepLR
+
+no_cuda = False
+use_cuda = not no_cuda and torch.cuda.is_available()
+torch.manual_seed(1)
+device = torch.device("cuda" if use_cuda else "cpu")
+print(device)
+batch_size = 64
+
+kwargs = {'batch_size': 64}
+if use_cuda:
+  kwargs.update({'num_workers': 1,
+                       'pin_memory': True,
+                       'shuffle': True},
+                     )
+
+transform=transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.1307,), (0.3081,))
+        ])
+dataset1 = datasets.MNIST('./', train=True, download=True,
+                       transform=transform)
+dataset2 = datasets.MNIST('./', train=False,
+                       transform=transform)
+train_loader = torch.utils.data.DataLoader(dataset1,**kwargs)
+test_loader = torch.utils.data.DataLoader(dataset2, **kwargs)
+
+images, labels = next(iter(train_loader))
+#plt.imshow(images[43].reshape(28,28), cmap="gray")
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, 3, 1) #in_channel x out_channel x kernel_size x stride
+        self.conv2 = nn.Conv2d(32, 64, 3, 1)
+        self.dropout1 = nn.Dropout2d(0.25)
+        self.dropout2 = nn.Dropout2d(0.5)
+        self.fc1 = nn.Linear(9216, 128)
+        self.fc2 = nn.Linear(128, 10)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2)
+        x = self.dropout1(x)
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.dropout2(x)
+        x = self.fc2(x)
+        output = F.log_softmax(x, dim=1)
+        return output
+
+def train(model, device, train_loader, optimizer, epoch):
+    pass
+
+def test(model, device, test_loader):
+    pass
+
+def main():
+
+	nepochs = 5 # Can change it
+	train_loss = []
+	test_loss= []
+	# Fill in before the epoch loop to instantiate the network model and different optimizers.
+	# Also use Step wise decay Learning rate scheduler
+	for epoch in range(0,nepochs):
+	    train_loss.append(train(model, device, train_loader, optimizer, epoch))
+	    test_loss.append(test(model, device, test_loader))
+	    epochs.append(epoch)
+
+
+if __name__=='__main__':
+	main()
